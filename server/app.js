8bfe33f8ff65c24903151cb656ac995f33fdd533
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-10 17:36:55 +0200

    set up React, Tailwind, Node server, connect the database

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+const cors = require('cors');
+const dotenv = require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+})
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
