240baaade7046f57de4d75869b1787eba3f5bfe7
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-11 12:45:28 +0200

    arrage routes, provide access to the home endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+// import AuthLayout from './components/Auth/AuthLayout';
+// import Greeting from './components/Auth/Greeting';
+// import Login from './components/Auth/Login';
+// import Register from './components/Auth/Register';
+// import ResetPassword from './components/Auth/ResetPassword';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/auth/*" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="reset-password" element={<ResetPassword />} />
+        </Route> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
