8bcae1aa6beaf0a60f9097e13df8653371425f06
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-16 06:05:05 +0200

    create whereDidYouHear and adjust it as another route of parental register route

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import AuthLayout from './components/Auth/AuthLayout/AuthLayout';
 import Login from './components/Auth/Login/Login';
 import Register from './components/Auth/Register/Register';
 import ResetPassword from './components/Auth/ResetPassword/ResetPassword';
-
+import WhereDidYouHear from './components/Auth/WhereDidYouHear/WhereDidYouHear';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<AuthLayout />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
-        <Route path="/auth/reset-password" element={<ResetPassword />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
+            <Route path="/auth/register/where-did-you-hear" element={<WhereDidYouHear />} />
+          <Route path="/auth/reset-password" element={<ResetPassword />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Auth/WhereDidYouHear/WhereDidYouHear.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './WhereDidYouHear.css'
+import Logo from '../../Logo/Logo'
+export default function WhereDidYouHear() {
+  return (
+    <div className='WhereDidYouHear w-full h-full'>
+        <header className='WhereDidYouHear_header w-full h-8 mb-3'>
+          <Logo />
+        </header>
+    </div>
+  )
+}
