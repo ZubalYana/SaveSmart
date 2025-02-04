85fe41fe6ad95993f2631269236782259192959e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-02-04 06:23:14 +0200

    install framer-motion library, create and animate the underlogo line

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 import LightLogo from '../LightLogo/LightLogo';
+
 export default function SideMenu() {
   return (
     <div className='SideMenu w-[270px] h-[90vh] bg-mainBlue p-8 rounded-br-3xl'>
       <LightLogo />
+      <motion.div
+        className="animatedLine bg-customWhite h-[1px] mt-2"
+        initial={{ width: 0 }}
+        animate={{ width: "116%" }}
+        transition={{ duration: 1, ease: "easeOut" }}
+      />
     </div>
-  )
+  );
 }
