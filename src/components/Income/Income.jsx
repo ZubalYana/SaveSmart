01694580307e7a1872d565ddd1f4c88164998f75
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-02-25 11:13:51 +0200

    create all the regular income required inputs and add fundamental styling

--- src/components/Income/Income.jsx
+++ src/components/Income/Income.jsx
@@ -5,6 +5,7 @@ import { Plus, ListChecks, Pencil, Clock3, Zap, ArrowLeft } from 'lucide-react';
 import Modal from 'react-modal';
 Modal.setAppElement('#root');
 import TextField from '@mui/material/TextField';
+import { Autocomplete } from "@mui/material";
 import Select from '@mui/material/Select';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
@@ -14,7 +15,110 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
-
+const CURRENCY_NAMES = {
+  "8": "Albanian Lek (ALL)",
+  "12": "Algerian Dinar (DZD)",
+  "32": "Argentine Peso (ARS)",
+  "36": "Australian Dollar (AUD)",
+  "48": "Bahraini Dinar (BHD)",
+  "50": "Bangladeshi Taka (BDT)",
+  "51": "Armenian Dram (AMD)",
+  "68": "Bolivian Boliviano (BOB)",
+  "72": "Botswana Pula (BWP)",
+  "96": "Brunei Dollar (BND)",
+  "108": "Burundi Franc (BIF)",
+  "116": "Cambodian Riel (KHR)",
+  "124": "Canadian Dollar (CAD)",
+  "144": "Sri Lankan Rupee (LKR)",
+  "152": "Chilean Peso (CLP)",
+  "156": "Chinese Yuan (CNY)",
+  "170": "Colombian Peso (COP)",
+  "188": "Costa Rican Colón (CRC)",
+  "191": "Croatian Kuna (HRK)",
+  "192": "Cuban Peso (CUP)",
+  "203": "Czech Koruna (CZK)",
+  "208": "Danish Krone (DKK)",
+  "230": "Ethiopian Birr (ETB)",
+  "262": "Djiboutian Franc (DJF)",
+  "270": "Gambian Dalasi (GMD)",
+  "324": "Guinean Franc (GNF)",
+  "344": "Hong Kong Dollar (HKD)",
+  "352": "Icelandic Króna (ISK)",
+  "356": "Indian Rupee (INR)",
+  "360": "Indonesian Rupiah (IDR)",
+  "368": "Iraqi Dinar (IQD)",
+  "376": "Israeli New Shekel (ILS)",
+  "392": "Japanese Yen (JPY)",
+  "398": "Kazakhstani Tenge (KZT)",
+  "400": "Jordanian Dinar (JOD)",
+  "404": "Kenyan Shilling (KES)",
+  "410": "South Korean Won (KRW)",
+  "414": "Kuwaiti Dinar (KWD)",
+  "417": "Kyrgyzstani Som (KGS)",
+  "418": "Lao Kip (LAK)",
+  "422": "Lebanese Pound (LBP)",
+  "434": "Libyan Dinar (LYD)",
+  "454": "Malawian Kwacha (MWK)",
+  "458": "Malaysian Ringgit (MYR)",
+  "480": "Mauritian Rupee (MUR)",
+  "484": "Mexican Peso (MXN)",
+  "496": "Mongolian Tögrög (MNT)",
+  "504": "Moroccan Dirham (MAD)",
+  "512": "Omani Rial (OMR)",
+  "524": "Nepalese Rupee (NPR)",
+  "554": "New Zealand Dollar (NZD)",
+  "566": "Nigerian Naira (NGN)",
+  "578": "Norwegian Krone (NOK)",
+  "586": "Pakistani Rupee (PKR)",
+  "600": "Paraguayan Guaraní (PYG)",
+  "604": "Peruvian Sol (PEN)",
+  "608": "Philippine Peso (PHP)",
+  "634": "Qatari Riyal (QAR)",
+  "643": "Russian Ruble (RUB)",
+  "682": "Saudi Riyal (SAR)",
+  "690": "Seychellois Rupee (SCR)",
+  "694": "Sierra Leonean Leone (SLL)",
+  "702": "Singapore Dollar (SGD)",
+  "704": "Vietnamese Đồng (VND)",
+  "710": "South African Rand (ZAR)",
+  "716": "Zimbabwean Dollar (ZWL)",
+  "748": "Eswatini Lilangeni (SZL)",
+  "752": "Swedish Krona (SEK)",
+  "756": "Swiss Franc (CHF)",
+  "764": "Thai Baht (THB)",
+  "784": "UAE Dirham (AED)",
+  "788": "Tunisian Dinar (TND)",
+  "800": "Ugandan Shilling (UGX)",
+  "818": "Egyptian Pound (EGP)",
+  "826": "British Pound Sterling (GBP)",
+  "834": "Tanzanian Shilling (TZS)",
+  "840": "US Dollar (USD)",
+  "858": "Uruguayan Peso (UYU)",
+  "860": "Uzbekistani Som (UZS)",
+  "886": "Yemeni Rial (YER)",
+  "901": "Taiwan Dollar (TWD)",
+  "928": "Venezuelan Bolívar (VES)",
+  "931": "Aruban Florin (AWG)",
+  "933": "Belarusian Ruble (BYN)",
+  "936": "Ghanaian Cedi (GHS)",
+  "941": "Serbian Dinar (RSD)",
+  "943": "Mozambican Metical (MZN)",
+  "946": "Romanian Leu (RON)",
+  "949": "Turkish Lira (TRY)",
+  "950": "CFA Franc BEAC (XAF)",
+  "952": "CFA Franc BCEAO (XOF)",
+  "968": "Surinamese Dollar (SRD)",
+  "969": "Malagasy Ariary (MGA)",
+  "971": "Afghan Afghani (AFN)",
+  "972": "Tajikistani Somoni (TJS)",
+  "973": "Congolese Franc (CDF)",
+  "975": "Bulgarian Lev (BGN)",
+  "976": "Macedonian Denar (MKD)",
+  "978": "Euro (EUR)",
+  "980": "Ukrainian Hryvnia (UAH)",
+  "985": "Polish Złoty (PLN)",
+  "986": "Brazilian Real (BRL)"
+}
 export default function Income() {
   const [isIncomeLoggingModalOpen, setisIncomeLoggingModalOpen] = useState(false);
   const [selectedIncomeType, setSelectedIncomeType] = useState(null);
@@ -23,6 +127,8 @@ export default function Income() {
   const [dayOfMonth, setDayOfMonth] = useState('');
   const [yearlyDate, setYearlyDate] = useState(dayjs());
   const [dayOfWeek, setDayOfWeek] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState(null);
+  const [savingMethod, setSavingMethod] = useState("");
 
   const openIncomeLoggingModal = () => {
     setisIncomeLoggingModalOpen(true);
@@ -32,6 +138,8 @@ export default function Income() {
     setDayOfMonth('');
     setYearlyDate(dayjs());
     setDayOfWeek('');
+    setSelectedCurrency(null);
+    setSavingMethod('');
   };
 
   const closeIncomeLoggingModal = () => setisIncomeLoggingModalOpen(false);
@@ -56,6 +164,7 @@ export default function Income() {
 
   const periodicityOptions = ['Daily', 'Weekly', 'Monthly', 'Yearly'];
   const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const savingMethods = ["Cash", "Card", "Bank Transfer", "Mobile Payment", "Cryptocurrency"];
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className='Income screen xs:p-4 md:p-6 lg:p-7'>
@@ -136,14 +245,14 @@ export default function Income() {
                 <ArrowLeft />
                 <span className='ml-2 text-defaultText'>Go back</span>
               </div>
-              <h3 className='mb-1 text-3xl text-mainBlue font-medium' style={{ fontFamily: 'Balsamiq Sans' }}>
+              <h3 className='mb-1 text-3xl text-mainBlue font-semibold' style={{ fontFamily: 'Balsamiq Sans' }}>
                 {selectedIncomeType === 'Regular income' ? 'Creating a Regular Income' : 'Logging an Irregular Income'}
               </h3>
               <p className='text-base text-defaultText'>Fill in the required information for your income.</p>
 
-              <div className="regularIncomeInputs">
-                <TextField id="outlined-basic" label="Income name (e.g. salary, scholarship)" variant="outlined" />
-                <FormControl sx={{ m: 1, width: 300 }}>
+              <div className="regularIncomeInputs w-full flex flex-wrap mt-5">
+                <TextField id="outlined-basic" label="Income name (e.g. salary, scholarship)" variant="outlined" className='w-[350px]' />
+                <FormControl sx={{width: 210, mx: 2 }}>
                   <InputLabel id="periodicity-label">Receiving periodicity</InputLabel>
                   <Select
                     labelId="periodicity-label"
@@ -157,7 +266,7 @@ export default function Income() {
                   </Select>
                 </FormControl>
                 {selectedPeriodicity === 'Weekly' && (
-                  <FormControl sx={{ m: 1, width: 300 }}>
+                  <FormControl sx={{width: 200}}>
                     <InputLabel id="day-of-week-label">Day of the week</InputLabel>
                     <Select
                       labelId="day-of-week-label"
@@ -180,7 +289,7 @@ export default function Income() {
                     value={dayOfMonth}
                     onChange={(e) => setDayOfMonth(e.target.value)}
                     variant="outlined"
-                    sx={{ m: 1, width: 300 }}
+                    sx={{width: 230}}
                   />
                 )}
                 {selectedPeriodicity === 'Yearly' && (
@@ -189,9 +298,43 @@ export default function Income() {
                     label="Select specific date"
                     value={yearlyDate}
                     onChange={(newValue) => setYearlyDate(newValue)}
-                    renderInput={(params) => <TextField {...params} sx={{ m: 1, width: 300 }} />}
+                    renderInput={(params) => <TextField {...params} sx={{width: 230}} />}
                   />
                 )}
+
+                <TextField
+                  id="receiving-sum"
+                  label="Receiving sum"
+                  type="number"
+                  inputProps={{ min: 1, max: 31 }}
+                  value={dayOfMonth}
+                  onChange={(e) => setDayOfMonth(e.target.value)}
+                  variant="outlined"
+                  sx={{width: 230}}
+                />
+                <Autocomplete
+                  value={selectedCurrency}
+                  onChange={(event, newValue) => setSelectedCurrency(newValue)}
+                  options={Object.keys(CURRENCY_NAMES)}
+                  getOptionLabel={(option) => CURRENCY_NAMES[option] || `Unknown (${option})`}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Saving currency" variant="outlined" />
+                  )}
+                  className="w-[250px]"
+                />
+                <Autocomplete
+                  value={savingMethod}
+                  onChange={(event, newValue) => setSavingMethod(newValue)}
+                  options={savingMethods}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Saving Method" variant="outlined" />
+                  )}
+                  className="w-[250px] mx-2"
+                />
+
+
+
+                
               </div>
             </div>
           )}
