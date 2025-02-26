e2b1260513e5b5c75a0735f7b871bd955631d59e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-02-26 05:21:43 +0200

    set up the income mongoose schema

---
+++ server/models/IncomeSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const IncomeSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    amount: { type: Number, required: true },
+    currency: { type: String, required: true },
+    method: { type: String, required: true }, 
+    isRegular: { type: Boolean, required: true },
+    periodicity: { type: String, enum: ['Daily', 'Weekly', 'Monthly', 'Yearly'], default: null },
+    dayOfMonth: { type: Number, default: null },
+    dayOfWeek: { type: String, default: null },
+    yearlyMonth: { type: Number, min: 1, max: 12, default: null }, 
+    yearlyDay: { type: Number, min: 1, max: 31, default: null },
+    dateReceived: { type: Date, required: function () { return !this.isRegular; } }, 
+    createdAt: { type: Date, default: Date.now },
+  });
+  
+  const Income = mongoose.model('Income', IncomeSchema);
+  module.exports = Income;
