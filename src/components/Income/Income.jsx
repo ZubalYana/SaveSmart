fd2173e3df380800c08f390d31fb3a8f2ba43ea0
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-02-27 11:04:59 +0200

    delete an unnecessary button on the incomes page

--- src/components/Income/Income.jsx
+++ src/components/Income/Income.jsx
@@ -236,7 +236,7 @@ export default function Income() {
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className='Income screen xs:p-4 md:p-6 lg:p-7'>
         <Burger />
-        <div className='w-[550px] flex justify-between'>
+        <div className='w-[360px] flex justify-between'>
           <button
             className='uppercase p-4 flex bg-accentLightBlue rounded-xl items-center justify-center text-sm font-medium text-customWhite transition-all duration-300 hover:bg-btnBgShade-500 hover:shadow-lg hover:scale-105 hover:bg-opacity-80'
             onClick={openIncomeLoggingModal}
@@ -247,11 +247,6 @@ export default function Income() {
           <button className='uppercase p-4 flex bg-accentLightBlue text-defaultText bg-opacity-30 rounded-xl items-center justify-center text-sm font-medium transition-all duration-300 hover:bg-btnBgShade-500 hover:text-customWhite hover:shadow-lg hover:scale-105 hover:bg-opacity-80'>
             <ListChecks className='mr-2' />
             Incomes list
-          </button>
-          <button 
-            className='uppercase p-4 flex bg-accentLightBlue text-defaultText bg-opacity-30 rounded-xl items-center justify-center text-sm font-medium transition-all duration-300 hover:bg-btnBgShade-500 hover:text-customWhite hover:shadow-lg hover:scale-105 hover:bg-opacity-80'>
-            <Pencil className='mr-2' size={20} />
-            Edit incomes
           </button>
         </div>
 
