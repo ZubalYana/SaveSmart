0ead62712dd94ddf7e087e3f9db7985ea48e214b
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-03-18 16:32:25 +0200

    display the basic user profile info

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -3,9 +3,10 @@ import "./MyProfile.css";
 import Burger from "../Burger/Burger";
 import LogoutButton from "../LogoutButton/LogoutButton";
 import { useQuery } from "@tanstack/react-query";
+import { Calendar, User } from "lucide-react";
 
 export default function MyProfile() {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
 
   const { data: user, isLoading, isError, error } = useQuery({
     queryKey: ["user"],
@@ -21,14 +22,45 @@ export default function MyProfile() {
       return response.json();
     },
   });
-  console.log(user);
-  
 
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  const formattedBirthday = new Date(user.dateOfBirth).toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
   return (
-    <div className="MyProfile screen xs:p-4 md:p-6 lg:p-7">
+    <div className="MyProfile screen xs:p-4 md:p-6 lg:p-7 text-defaultText">
       <Burger />
-      <h1 className="dynamicGreeting text-base font-medium xs:text-xl lg:text-2xl">User profile</h1>
+      <h1 className="dynamicGreeting text-base font-medium xs:text-xl lg:text-2xl text-black">
+        User profile
+      </h1>
+      <div className="w-[500px] flex mt-5 mb-5">
+        <img src="/userPicLayout.svg" alt="userPicLayout" />
+        <div className="ml-4">
+          <p className="text-2xl font-medium text-black">{user.name}</p>
+          <p className="text-base font-light mt-1">{user.email}</p>
+        </div>
+      </div>
+      <div className="w-[410px] flex justify-between mb-4">
+      <div className="flex items-center">
+        <Calendar />
+        <p className="text-base font-medium ml-2 xs:text-sm lg:text-base">
+          Birthday: <span className="text-black">{formattedBirthday}</span>
+        </p>
+      </div>
+      <div className="flex items-center">
+        <User />
+        <p className="text-base font-medium ml-2 xs:text-sm lg:text-base">
+          Plan: <span className="text-black">{user.purposeOfUsage}</span>
+        </p>
+      </div>
+      </div>
+
       <LogoutButton />
     </div>
   );
 }
+
